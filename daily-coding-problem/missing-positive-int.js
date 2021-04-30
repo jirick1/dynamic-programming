9070c20eb5363867c63d0eba575ba662624d0b16
James Irick <james.e.irick@hotmail.com>
2021-04-30 14:01:34 -0400

    added test

--- daily-coding-problem/missing-positive-int.js
+++ daily-coding-problem/missing-positive-int.js
@@ -28,10 +28,10 @@ var firstMissingPositive = function(nums) {
   return lowest;
 };
 
-// console.log(firstMissingPositive([3, 4, -1, 1]))      // expect 2
-// console.log(firstMissingPositive([1, 2, 0]))          // expect 3
-// console.log(firstMissingPositive([7, 8, 9, 11, 12]))  // expect 1
-// console.log(firstMissingPositive([1, 1000]))          // expect 2
-// console.log(firstMissingPositive([999,500,1]))        // expect 2
-// console.log(firstMissingPositive([2,1]))              // expect 3
+console.log(firstMissingPositive([3, 4, -1, 1]))      // expect 2
+console.log(firstMissingPositive([1, 2, 0]))          // expect 3
+console.log(firstMissingPositive([7, 8, 9, 11, 12]))  // expect 1
+console.log(firstMissingPositive([1, 1000]))          // expect 2
+console.log(firstMissingPositive([999,500,1]))        // expect 2
+console.log(firstMissingPositive([2,1]))              // expect 3
 console.log(firstMissingPositive([1, -1, 3, 2,1]))              // expect 3
